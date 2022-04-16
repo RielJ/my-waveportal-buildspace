b7003d07333c7d881ff44da79a0a9831d88348a8
RielJ <bulaybulay.rielj@gmail.com>
2022-04-16 22:29:34 +0800

    Section 4: Added Chance Mechanism.

--- apps/wave-portal/scripts/run.ts
+++ apps/wave-portal/scripts/run.ts
@@ -24,20 +24,23 @@ const main = async () => {
     hre.ethers.utils.formatEther(contractBalance)
   );
 
-  let waveTxn = await waveContract.wave("A message!");
+  /*
+   * Let's try two waves now
+   */
+  const waveTxn = await waveContract.wave("This is wave #1");
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
-  console.log({ waveCount });
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
 
-  waveTxn = await waveContract.connect(randomPerson).wave("Another Message!");
-  await waveTxn.wait();
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-  waveCount = await waveContract.getTotalWaves();
-  console.log({ waveCount });
-
-  const wavers = await waveContract.getAllWaves();
-  console.log({ wavers });
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
